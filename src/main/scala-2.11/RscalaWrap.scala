fbbaca86487d93b5ccf9bfb45e8570ce6b5b5508
Arthur Lui <luiarthur@gmail.com>
2016-06-06 13:17:25 -0700

    yes

--- src/main/scala-2.11/RscalaWrap.scala
+++ src/main/scala-2.11/RscalaWrap.scala
@@ -4,4 +4,17 @@ import breeze.linalg._
 
 object RClient2 {
   val R =  org.ddahl.rscala.callback.RClient()
+  R.eval("plot(rnorm)")
+  R eval """
+    plot(rnorm(100))
+  """
+  R eval "plot(rnorm(100))"
+  R eval "pairs(matrix(rnorm(100),25,4))"
+  R evalD0 "x"
+
+  val M = R evalD2 "matrix(rnorm(100),ncol=4)"
+  val X = DenseMatrix.tabulate(25,4){ (i,j) => M(i)(j) }
+
+  R eval "print(iris)"
+  R evalD1 "iris[,1]"
 }
