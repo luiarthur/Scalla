5356de49561c2da95e410928b7f3040a94437fe4
Arthur Lui <luiarthur@gmail.com>
2016-06-06 00:36:59 -0700

    yes

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -6,26 +6,37 @@ object Main extends App {
     - source functions conveniently from scala
     - conveniently get and put variables to and from scala
 
-  For example: 
+    For example: 
 
-    R> sourceScala("scalaFunc.scala")
-  
-  should read the file scalaFunc.scala and source the contents.
-  The user should then be able to use the function like an R function.
-  That is the function should be wrapped.
+      R> sourceScala("scalaFunc.scala")
+    
+    should read the file scalaFunc.scala and source the contents.
+    The user should then be able to use the function like an R function.
+    That is the function should be wrapped.
 
-    R> x <- scalaGet("x")
-    R> scalaVal(x,"y")
-    R> scalaVar(x,"y")
+      R> x <- scalaGet("x")
+      R> scalaVal(x,"y")
+      R> scalaVar(x,"y")
 
-  should first read a variable from scala (of any type) into (x)
-  then put x into a scala val (or var) called "y"
+    should first read a variable from scala (of any type) into (x)
+    then put x into a scala val (or var) called "y"
 
 
-  I want to in Scala: 
+  I want to in Scala (like in RCall.jl):
+    - run any R function I want
+      
+      For Example:
+        scala> val R =  org.ddahl.rscala.callback.RClient()
+        scala> val plot = R get plot
+        scala> plot(x,y)
 
-    scala> rsource("rfile.R")
-    scala> val x = rfunction(a,b,c)
+      should yield a plot
+
+    - Realistically, try doing this for the plot function only
+    - allow arguments to be 
+        - Matrix, Array, scalar, 
+        - DenseMatrix, DenseVector 
+      of any type
 
   """ + Console.RESET)
 }
